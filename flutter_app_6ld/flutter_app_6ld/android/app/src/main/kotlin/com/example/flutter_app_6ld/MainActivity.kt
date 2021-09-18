2f7a7f343eff4706fbbd1fd419c2cbe85829d649
Tomas <tomas.butkevicius99@gmail.com>
2021-09-18 17:10:08 +0300

    add 6LD

---
+++ flutter_app_6ld/flutter_app_6ld/android/app/src/main/kotlin/com/example/flutter_app_6ld/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_6ld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app_6ld/flutter_app_6ld/lib/services/time_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TimeService {
+  static int getDifferenceOfTime(TimeOfDay timeA, TimeOfDay timeB) {
+    final int differenceInHours = (timeA.hour - timeB.hour).abs();
+    int differenceInMinutes = (timeA.minute - timeB.minute).abs();
+
+    return (differenceInHours * 60) + differenceInMinutes;
+  }
+
+  static TimeOfDay getCurrentTime() => TimeOfDay.now();
+  
+  static Future<TimeOfDay?> timeSelector(BuildContext context) {
+    return showTimePicker(
+      context: context,
+      initialTime: getCurrentTime(),
+    );
+  }
+
+}
