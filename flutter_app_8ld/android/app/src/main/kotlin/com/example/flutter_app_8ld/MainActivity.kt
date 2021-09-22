2c4f0eec3d9bdd8264635848fda6b3c515599213
Tomas <tomas.butkevicius99@gmail.com>
2021-09-22 22:03:57 +0300

    initial commit

---
+++ flutter_app_8ld/android/app/src/main/kotlin/com/example/flutter_app_8ld/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_8ld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter_app_9ld/android/app/src/main/kotlin/com/example/flutter_app_9ld/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_app_9ld
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
